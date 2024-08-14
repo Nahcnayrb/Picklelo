279b445b4c90641f2adf93a1d476b9c20d0a6d93
bryan <bryanchan22tr@gmail.com>
2024-08-14 01:26:20 -0700

    made change

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-//axios.defaults.baseURL='http://192.168.1.97:8080/'
-//axios.defaults.baseURL='https://picklelo-backend.onrender.com/'
+//axios.defaults.baseURL='http://192.168.1.97:8000/'
+axios.defaults.baseURL='https://picklelo-backend.onrender.com/'
 //axios.defaults.baseURL='https://resident-caty-picklelo-7b1f1d19.koyeb.app/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
