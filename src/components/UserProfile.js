47b20271143dae04c9e95431dd7726d529e3503f
bryan <bryanchan22tr@gmail.com>
2024-12-08 20:45:23 -0800

    added functionality to remove and add pfps in settings

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,11 +2,13 @@ import { useEffect } from "react"
 import { useState } from 'react'
 import { Button } from '@mui/material'
 import pfp from "./0140.png"
+import defaultpfp from "./0617.png"
 
 export default function UserProfile(props) {
 
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
+    const [profilePicture, setProfilePicture] = useState()
 
 
     useEffect(()=> {
@@ -15,13 +17,40 @@ export default function UserProfile(props) {
 
             setEmail(props.user.email)
             setName(props.user.name)
+            setProfilePicture(pfp)
         }
 
 
     
-      },[props.user])
+        },[props.user]
+    )
 
-      // name(editable), username(locked), email(locked), profile picture(editable)
+    let handleFileChange = (e) => {
+
+        if (e.target.files[0]) {
+            
+            
+            let file = URL.createObjectURL(e.target.files[0])
+            console.log(e.target.files[0].type)
+            console.log(file)
+            setProfilePicture(file)
+
+            
+
+        } else {
+            // case no file provided
+            // update file state
+            // setProfilePicture()
+        }
+
+    }
+
+
+    
+
+
+    
+
 
     
 
@@ -52,14 +81,22 @@ export default function UserProfile(props) {
                         <input type='text' className="form-control" value={email} onChange={(e) => setEmail(e.target.value)}/>
                         <div className="form-padding"/>
                         <label className="form-label">Profile Picture</label>
-                        <div style={{display: "inline-block"}}>
-                            <img src={pfp} className='pfp' style={{marginTop: "0.5rem"}}></img>
-                            <input type='file' style={{width: "100px", marginLeft: "20px"}}></input>
+                        <div style={{display: "flex"}}>
+                            <img src={!profilePicture?defaultpfp:profilePicture} className='pfp' style={{marginTop: "0.5rem"}}></img>
+                            <div style={{width: "150px", marginLeft: "20px", marginTop: "15px"}}>
+                            <input type='file' style={{width: "100px", marginLeft: "20px"}} onChange={e => handleFileChange(e)}></input>
+                            <input type='button' 
+                                disabled={!profilePicture} 
+                                value={"Remove Picture"} 
+                                style={{width: "150px", marginTop: "20px"}}
+                                onClick={e => setProfilePicture()}
+                            ></input>
+                            </div>
                         </div>
                         
                         <div className="form-padding"/>
                         <div className="form-padding"/>
-                        <Button type="submit" variant="contained" style={{backgroundColor: "#006400", width: "110px", marginLeft: "2.8rem"}}>
+                        <Button type="submit" variant="contained" style={{backgroundColor: "#006400", width: "110px", marginLeft: "4.7rem"}}>
                                 <label className='form-label' style={{cursor: "pointer", paddingTop: "8px"}}>SAVE</label>
                         </Button>
                     </div>
